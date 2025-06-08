d64d99b03526393b3b64d0c7b3a92509d0da9423
vincent-guizot <vincentguizot128@gmail.com>
2025-06-08 22:24:20 +0700

    1

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,18 +1,24 @@
+"use client";
 import Image from "next/image";
 import React from "react";
-import UNCVRDProduction from "@/assets/images/logo.png";
 
+import { motion } from "framer-motion";
+
+const MotionImage = motion(Image);
 export default function Banner() {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-stone-900">
       <div className="w-full">
-        <div className="w-full p-3 banner-bg">
+        <div className="w-full p-12 banner-bg">
           <div className="h-full flex items-center justify-center">
-            <Image
-              width={512}
+            <MotionImage
+              width={576}
               height={0}
               src="/assets/images/logo_tr.png"
               alt="Logo"
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
             />
           </div>
         </div>
